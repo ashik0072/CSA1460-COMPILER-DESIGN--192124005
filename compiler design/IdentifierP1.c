d1c5df1e1b4c71b2d948e4fc52d034337ecba051
ashik0072 <114140750+ashik0072@users.noreply.github.com>
2022-10-04 11:44:19 +0530

    Add files via upload

---
+++ compiler design/IdentifierP1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h> 
+#include<ctype.h>
+void main()
+{
+char a[10]; int flag, i=1; clrscr();
+printf("\n Enter an identifier:"); gets(a);
+if(isalpha(a[0]))
+flag=1; else
+printf("\n Not a valid identifier");
+while(a[i]!='\0')
+{
+if(!isdigit(a[i])&&!isalpha(a[i]))
+{
+flag=0; break;
+} i++;
+}
+if(flag==1)
+printf("\n Valid identifier"); getch();
+}
